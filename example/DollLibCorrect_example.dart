bf66ecafff99e38755c0da5f3820a48b5e4ceabd
Jenny <gtg287y@gmail.com>
2017-12-10 10:10:50 -0500

    dependencies

--- example/DollLibCorrect_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:DollLibCorrect/DollLibCorrect.dart';
-
-main() {
-  var awesome = new Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- lib/src/DollRenderer_base.dart
+++ lib/src/DollRenderer_base.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2017, Jenny. All rights reserved. Use of this source code
+// Copyright (c) 2017, JR. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 // TODO: Put public facing types in this file.
