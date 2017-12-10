06a431724d364584abacaa38f9e0426df94d9468
Jenny <gtg287y@gmail.com>
2017-12-10 11:14:47 -0500

    can test dolls before deploying

---
+++ example/main.dart
@@ -0,0 +1,20 @@
+import 'dart:html';
+import 'package:DollLibCorrect/DollRenderer.dart';
+import 'dart:async';
+
+Doll doll;
+void main() {
+    querySelector('#output').text = 'Your Dart app is running.';
+    doll = Doll.makeRandomDoll();
+    drawDoll();
+}
+
+Future<bool>  drawDoll() async{
+    Element innerDiv   = new DivElement();
+    CanvasElement finishedProduct = new CanvasElement(width: doll.width, height: doll.height);
+    innerDiv.className = "cardWithForm";
+    await Renderer.drawDoll(finishedProduct, doll);
+    finishedProduct.className = "cardCanvas";
+    innerDiv.append(finishedProduct);
+    querySelector('#output').append(innerDiv);
+}
