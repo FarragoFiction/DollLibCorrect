44e83b03e5160dde15d379ed32f24be0c49e5d36
Jenny <gtg287y@gmail.com>
2018-01-05 21:22:16 -0500

    shit ton of horns

--- lib/src/Dolls/HomestuckTrollDoll.dart
+++ lib/src/Dolls/HomestuckTrollDoll.dart
@@ -33,7 +33,7 @@ class HomestuckTrollDoll extends HomestuckDoll {
     //these bodies look terrible with troll signs. if any of these use 47,48, or 49
     List<int> bannedRandomBodies = <int>[96,219,221,223,5,11,14,43,50,59,65,66,67,70,72,75,74,98,100,101,102,106,107,109,63,17];
     int defaultBody = 48;
-    int maxHorn = 177;
+    int maxHorn = 208;
     int maxFin = 19;
     int maxCanonSymbol = 288; //288 eventually
     int maxWing = 13;
