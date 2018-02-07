0d10c8c8a8fb567872d679bd5645e6e4e14317fc
Jenny <gtg287y@gmail.com>
2018-02-06 22:59:38 -0500

    trolls wont' use new bodies that block sign

--- lib/src/Dolls/HomestuckTrollDoll.dart
+++ lib/src/Dolls/HomestuckTrollDoll.dart
@@ -31,7 +31,7 @@ class HomestuckTrollDoll extends HomestuckDoll {
     //Don't go over 255 for any old layer unless you want to break shit. over 255 adds an exo.
 
     //these bodies look terrible with troll signs. if any of these use 47,48, or 49
-    List<int> bannedRandomBodies = <int>[96,219,221,223,5,11,14,43,50,59,65,66,67,70,72,75,74,98,100,101,102,106,107,109,63,17];
+    List<int> bannedRandomBodies = <int>[226,227,230,96,219,221,223,5,11,14,43,50,59,65,66,67,70,72,75,74,98,100,101,102,106,107,109,63,17];
     //if a troll or grub has these eyes, they will be mutant
     List<int> mutantEyeList = <int>[2,11,31,44,46,47,85];
     int defaultBody = 48;
