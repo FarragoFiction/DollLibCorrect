f0b155fbe2390b688d6d5b4893f1bb721f2a3a42
Jenny <gtg287y@gmail.com>
2018-03-17 23:00:55 -0400

    more debug

--- lib/src/Dolls/Doll.dart
+++ lib/src/Dolls/Doll.dart
@@ -93,7 +93,10 @@ abstract class Doll {
         for(SpriteLayer sourceLayer in source.dataOrderLayers) {
             for(SpriteLayer replacementLayer in replacement.dataOrderLayers) {
                 if(sourceLayer.imgNameBase == replacementLayer.imgNameBase) {
+                    print("${sourceLayer.imgNameBase} is ${replacementLayer.imgNameBase}");
                     replacementLayer.imgNumber = sourceLayer.imgNumber;
+                }else {
+                    print("${sourceLayer.imgNameBase} is not ${replacementLayer.imgNameBase}");
                 }
             }
         }
