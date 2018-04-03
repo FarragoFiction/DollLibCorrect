75c63001ba241684e74746ec6a9e25ab4fec7ce4
Jenny <gtg287y@gmail.com>
2018-04-02 23:20:27 -0400

    two hairs were broken

--- lib/src/Dolls/HomestuckDoll.dart
+++ lib/src/Dolls/HomestuckDoll.dart
@@ -19,7 +19,7 @@ class HomestuckDoll extends Doll {
     String relativefolder = "images/Homestuck";
     //Don't go over 255 for any old layer unless you want to break shit. over 255 adds an exo.
     final int maxBody = 281;
-    final int maxHair = 222;
+    final int maxHair = 220;
     final int maxEye = 164;
     final int maxMouth = 159;
     final int maxSymbol = 250;
