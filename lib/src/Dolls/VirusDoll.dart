d4df2600ad8f88b576ad97e2a053b06fc403c40f
Jenny <gtg287y@gmail.com>
2018-04-02 22:01:18 -0400

    virus colors

--- lib/src/Dolls/VirusDoll.dart
+++ lib/src/Dolls/VirusDoll.dart
@@ -45,29 +45,43 @@ class VirusDoll extends Doll{
   List<SpriteLayer>  get dataOrderLayers => <SpriteLayer>[leg1, leg2, leg3, leg4, decoLegs, capsid, body];
 
   @override
+  Palette paletteSource = new VirusPalette()
+    ..armor1 = '#00fffa'
+    ..armor2 = '#00d6d2'
+    ..armor3 = '#00a8a5'
+    ..claw1 = '#76e0db'
+    ..claw2 = '#9bc9c7'
+    ..capsid1 = '#0000ff'
+    ..capsid2 = '#0000c4'
+    ..capsid3 = '#000096'
+    ..capsid4 = '#5151ff'
+    ..accent1 = '#8700ff'
+    ..accent2 = '#a84cff';
+  @override
   Palette palette = new VirusPalette()
-    ..accent = '#FF9B00'
-    ..aspect_light = '#FF9B00'
-    ..aspect_dark = '#FF8700'
-    ..shoe_light = '#7F7F7F'
-    ..shoe_dark = '#727272'
-    ..cloak_light = '#A3A3A3'
-    ..cloak_mid = '#999999'
-    ..cloak_dark = '#898989'
-    ..shirt_light = '#EFEFEF'
-    ..shirt_dark = '#DBDBDB'
-    ..pants_light = '#C6C6C6'
-    ..eye_white_left = '#ffffff'
-    ..eye_white_right = '#ffffff'
-    ..pants_dark = '#ADADAD'
-    ..hair_main = '#ffffff'
-    ..hair_accent = '#ADADAD'
-    ..skin = '#ffffff';
+    ..armor1 = '#FF9B00'
+    ..armor2 = '#FF9B00'
+    ..armor3 = '#FF8700'
+    ..claw1 = '#7F7F7F'
+    ..claw2 = '#727272'
+    ..capsid1 = '#A3A3A3'
+    ..capsid2 = '#999999'
+    ..capsid3 = '#898989'
+    ..capsid4 = '#EFEFEF'
+    ..accent1 = '#DBDBDB'
+    ..accent2 = '#C6C6C6';
 
 
   VirusDoll() {
     initLayers();
     randomize();
+  }
+
+  @override
+  void randomizeNotColors() {
+    for(SpriteLayer l in renderingOrderLayers) {
+      l.imgNumber = rand.nextInt(l.maxImageNumber+1);
+    }
   }
 
   @override
@@ -110,23 +124,18 @@ class VirusDoll extends Doll{
 
 /// Convenience class for getting/setting aspect palettes
 class VirusPalette extends Palette {
-  static String _ACCENT = "accent";
-  static String _ASPECT_LIGHT = "aspect1";
-  static String _ASPECT_DARK = "aspect2";
-  static String _SHOE_LIGHT = "shoe1";
-  static String _SHOE_DARK = "shoe2";
-  static String _CLOAK_LIGHT = "cloak1";
-  static String _CLOAK_MID = "cloak2";
-  static String _CLOAK_DARK = "cloak3";
-  static String _SHIRT_LIGHT = "shirt1";
-  static String _SHIRT_DARK = "shirt2";
-  static String _PANTS_LIGHT = "pants1";
-  static String _PANTS_DARK = "pants2";
-  static String _HAIR_MAIN = "hairMain";
-  static String _HAIR_ACCENT = "hairAccent";
-  static String _EYE_WHITE_LEFT = "eyeWhitesLeft";
-  static String _EYE_WHITE_RIGHT = "eyeWhitesRight";
-  static String _SKIN = "skin";
+
+  static String ARMOR1 = "accent";
+  static String ARMOR2 = "aspect1";
+  static String ARMOR3 = "aspect2";
+  static String CLAW1 = "shoe1";
+  static String CLAW2 = "shoe2";
+  static String CAPSID1 = "cloak1";
+  static String CAPSID2 = "cloak2";
+  static String CAPSID3 = "cloak3";
+  static String CAPSID4 = "shirt1";
+  static String ACCENT1 = "shirt2";
+  static String ACCENT2 = "pants1";
 
   static Colour _handleInput(Object input) {
     if (input is Colour) {
@@ -148,73 +157,51 @@ class VirusPalette extends Palette {
     throw "Invalid AspectPalette input: colour must be a Colour object, a valid colour int, or valid hex string (with or without leading #)";
   }
 
-  Colour get text => this[_ACCENT];
 
-  Colour get accent => this[_ACCENT];
 
-  void set accent(dynamic c) => this.add(_ACCENT, _handleInput(c), true);
+  Colour get armor1 => this[ARMOR1];
 
-  Colour get aspect_light => this[_ASPECT_LIGHT];
+  void set armor1(dynamic c) => this.add(ARMOR1, _handleInput(c), true);
 
-  void set aspect_light(dynamic c) => this.add(_ASPECT_LIGHT, _handleInput(c), true);
+  Colour get armor2 => this[ARMOR2];
 
-  Colour get aspect_dark => this[_ASPECT_DARK];
+  void set armor2(dynamic c) => this.add(ARMOR2, _handleInput(c), true);
 
-  void set aspect_dark(dynamic c) => this.add(_ASPECT_DARK, _handleInput(c), true);
+  Colour get armor3 => this[ARMOR3];
 
-  Colour get shoe_light => this[_SHOE_LIGHT];
+  void set armor3(dynamic c) => this.add(ARMOR3, _handleInput(c), true);
 
-  void set shoe_light(dynamic c) => this.add(_SHOE_LIGHT, _handleInput(c), true);
+  Colour get claw1 => this[CLAW1];
 
-  Colour get shoe_dark => this[_SHOE_DARK];
+  void set claw1(dynamic c) => this.add(CLAW1, _handleInput(c), true);
 
-  void set shoe_dark(dynamic c) => this.add(_SHOE_DARK, _handleInput(c), true);
+  Colour get claw2 => this[CLAW2];
 
-  Colour get cloak_light => this[_CLOAK_LIGHT];
+  void set claw2(dynamic c) => this.add(CLAW2, _handleInput(c), true);
 
-  void set cloak_light(dynamic c) => this.add(_CLOAK_LIGHT, _handleInput(c), true);
+  Colour get capsid1 => this[CAPSID1];
 
-  Colour get cloak_mid => this[_CLOAK_MID];
+  void set capsid1(dynamic c) => this.add(CAPSID1, _handleInput(c), true);
 
-  void set cloak_mid(dynamic c) => this.add(_CLOAK_MID, _handleInput(c), true);
+  Colour get capsid2 => this[CAPSID2];
 
-  Colour get cloak_dark => this[_CLOAK_DARK];
+  void set capsid2(dynamic c) => this.add(CAPSID2, _handleInput(c), true);
 
-  void set cloak_dark(dynamic c) => this.add(_CLOAK_DARK, _handleInput(c), true);
+  Colour get capsid3 => this[CAPSID3];
 
-  Colour get shirt_light => this[_SHIRT_LIGHT];
+  void set capsid3(dynamic c) => this.add(CAPSID3, _handleInput(c), true);
 
-  void set shirt_light(dynamic c) => this.add(_SHIRT_LIGHT, _handleInput(c), true);
+  Colour get capsid4 => this[CAPSID4];
 
-  Colour get shirt_dark => this[_SHIRT_DARK];
+  void set capsid4(dynamic c) => this.add(CAPSID4, _handleInput(c), true);
 
-  void set shirt_dark(dynamic c) => this.add(_SHIRT_DARK, _handleInput(c), true);
+  Colour get accent1 => this[ACCENT1];
 
-  Colour get pants_light => this[_PANTS_LIGHT];
+  void set accent1(dynamic c) => this.add(ACCENT1, _handleInput(c), true);
 
-  void set pants_light(dynamic c) => this.add(_PANTS_LIGHT, _handleInput(c), true);
+  Colour get accent2 => this[ACCENT2];
 
-  Colour get pants_dark => this[_PANTS_DARK];
+  void set accent2(dynamic c) => this.add(ACCENT2, _handleInput(c), true);
 
-  void set pants_dark(dynamic c) => this.add(_PANTS_DARK, _handleInput(c), true);
 
-  Colour get hair_main => this[_HAIR_MAIN];
-
-  void set hair_main(dynamic c) => this.add(_HAIR_MAIN, _handleInput(c), true);
-
-  Colour get hair_accent => this[_HAIR_ACCENT];
-
-  void set hair_accent(dynamic c) => this.add(_HAIR_ACCENT, _handleInput(c), true);
-
-  Colour get eye_white_left => this[_EYE_WHITE_LEFT];
-
-  void set eye_white_left(dynamic c) => this.add(_EYE_WHITE_LEFT, _handleInput(c), true);
-
-  Colour get eye_white_right => this[_EYE_WHITE_RIGHT];
-
-  void set eye_white_right(dynamic c) => this.add(_EYE_WHITE_RIGHT, _handleInput(c), true);
-
-  Colour get skin => this[_SKIN];
-
-  void set skin(dynamic c) => this.add(_SKIN, _handleInput(c), true);
 }
