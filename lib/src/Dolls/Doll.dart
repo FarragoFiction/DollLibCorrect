be986ceaab4a665c4e744f05931e40e9aa5fb4b1
Jenny <gtg287y@gmail.com>
2018-04-30 15:18:43 -0400

    fix

--- lib/src/Dolls/Doll.dart
+++ lib/src/Dolls/Doll.dart
@@ -9,7 +9,7 @@ import "../../DollRenderer.dart";
 abstract class Doll {
 
     //useful for the builder
-    static List<int> allDollTypes = <int>[1,2,16,12,13,3,4,7,9,10,14,113,15,8,151,17,18,19];
+    static List<int> allDollTypes = <int>[1,2,16,12,13,3,4,7,9,10,14,113,15,8,151,17,18,19,20];
 
     //in case i want controlled random
     Random rand = new Random();
