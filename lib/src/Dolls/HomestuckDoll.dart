4f4d287fad7800829a16f99c2416c000603b07bd
Jenny <gtg287y@gmail.com>
2018-05-07 13:46:00 -0400

    hair

--- lib/src/Dolls/HomestuckDoll.dart
+++ lib/src/Dolls/HomestuckDoll.dart
@@ -24,7 +24,7 @@ class HomestuckDoll extends Doll {
     String relativefolder = "images/Homestuck";
     //Don't go over 255 for any old layer unless you want to break shit. over 255 adds an exo.
     final int maxBody = 307;
-    final int maxHair = 237;
+    final int maxHair = 240;
     final int maxEye = 176;
     final int maxMouth = 169;
     final int maxSymbol = 250;
