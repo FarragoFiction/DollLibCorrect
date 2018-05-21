910c9aba5c5b4d0096908c62fa41b478d7180585
Jenny <gtg287y@gmail.com>
2018-05-21 13:37:28 -0400

    all kid parts shouldu be in

--- lib/src/Dolls/HomestuckDoll.dart
+++ lib/src/Dolls/HomestuckDoll.dart
@@ -24,10 +24,10 @@ class HomestuckDoll extends HatchableDoll {
     @override
     String relativefolder = "images/Homestuck";
     //Don't go over 255 for any old layer unless you want to break shit. over 255 adds an exo.
-    final int maxBody = 307;
-    final int maxHair = 240;
-    final int maxEye = 176;
-    final int maxMouth = 169;
+    final int maxBody = 331;
+    final int maxHair = 263;
+    final int maxEye = 186;
+    final int maxMouth = 177;
     final int maxSymbol = 250;
     final int maxGlass = 135;
     final int maxGlass2 = 152;
