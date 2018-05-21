71f76f990afe6e67e45d7df48bd7f52711b8b1cd
Jenny <gtg287y@gmail.com>
2018-05-21 14:19:14 -0400

    tested talk sprite

--- lib/src/Dolls/TalkSpriteDoll.dart
+++ lib/src/Dolls/TalkSpriteDoll.dart
@@ -33,7 +33,7 @@ class TalkSpriteDoll extends Doll{
   final int maxSymbol = 15;
   final int maxBrows = 2;
   final int maxEyes = 3;
-  final int maxHair = 7;
+  final int maxHair = 5;
   final int maxHood = 11;
   final int maxMouth = 3;
   final int maxNose = 2;
