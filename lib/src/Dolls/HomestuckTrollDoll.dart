8b10d4d576526b6178c20e02a0cf3b9f5741f348
Jenny <gtg287y@gmail.com>
2018-06-01 15:51:01 -0400

    removed non png

--- lib/src/Dolls/HomestuckTrollDoll.dart
+++ lib/src/Dolls/HomestuckTrollDoll.dart
@@ -41,7 +41,7 @@ class HomestuckTrollDoll extends HomestuckDoll {
     //if a troll or grub has these eyes, they will be mutant
     List<int> mutantEyeList = <int>[2,11,31,44,46,47,85];
     int defaultBody = 48;
-    int maxHorn = 298;
+    int maxHorn = 297;
     int maxFin = 21;
     int maxCanonSymbol = 288; //288 eventually
     int maxWing = 64;
