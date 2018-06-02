82ea3665b20302bf86d4ae927668aef5e0ec32f2
Jenny <gtg287y@gmail.com>
2018-06-02 13:30:47 -0400

    fighting git

--- example/main.dart
+++ example/main.dart
@@ -14,7 +14,8 @@ void main() {
 Future<bool> start() async {
     await Loader.preloadManifest();
     print("done awaiting");
-    doll =  Doll.randomDollOfType(23);
+    //doll =  Doll.randomDollOfType(23);
+    doll = Doll.loadSpecificDoll("AqA7qIbMBGhiAjDJBGgaB4hjAjD51035103JBGgCAABYWlrS0dGJiIgAAACIiIiYmZkhJibOBGhiAjABCAgICKADAAAICAgIAPAIAAgL4OICAAAAAA==");
     //doll = new HomestuckDoll();
     //doll = Doll.loadSpecificDoll("http://localhost:63342/DollBuilder/web/index.html?ElD_ZzJ7McjwHn6YIEx4Gj5MEiiqKFR6HDzWEFKUEDA6rlwibjoCBAIEBAAAAA==");
     //doll = Doll.randomDollOfType(18);
