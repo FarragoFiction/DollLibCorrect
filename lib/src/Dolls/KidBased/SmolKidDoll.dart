5fc3ccb81d7361fd3209917ec35b783e4d073ba0
jadedResearcher <gtg287y@gmail.com>
2018-09-08 19:07:35 -0400

    need to fiddle

--- lib/src/Dolls/KidBased/SmolKidDoll.dart
+++ lib/src/Dolls/KidBased/SmolKidDoll.dart
@@ -54,29 +54,6 @@ class SmolKidDoll extends HomestuckDoll {
         //print("initializing layers, folder is: $folder and use absolute path is $useAbsolutePath");
 
         extendedBody = new SpriteLayer("SmolBody","$folder/SmolBody/", 1, maxBody);
-        /*
-         hairTop = new SpriteLayer("HairOld","$folder/HairTop/", 1, 255);
-        hairBack = new SpriteLayer("HairOld","$folder/HairBack/", 1, 255);
-        //hairTop.syncedWith.add(hairBack);
-       // hairBack.slave = true; //can't be selected on it's own
-
-        extendedHairTop = new SpriteLayer("HairFront","$folder/HairTop/", 1, maxHair, supportsMultiByte: true)..secretMax = maxSecretHair;
-        extendedHairBack = new SpriteLayer("HairBack","$folder/HairBack/", 1, maxHair, syncedWith:<SpriteLayer>[extendedHairTop], supportsMultiByte: true)..secretMax = maxSecretHair;
-        extendedHairTop.syncedWith.add(extendedHairBack);
-        extendedHairBack.slave = true;
-
-        extendedBody = new SpriteLayer("Body","$folder/Body/", 0, maxBody, supportsMultiByte: true)..secretMax = maxSecretBody;
-        body = new SpriteLayer("BodyOld","$folder/Body/", 0, 255);
-
-        facePaint = new SpriteLayer("FacePaint","$folder/FacePaint/", 0, maxFacePaint);
-
-        symbol = new SpriteLayer("Symbol","$folder/Symbol/", 1, maxSymbol)..secretMax = maxSecretSymbol;
-        mouth = new SpriteLayer("Mouth","$folder/Mouth/", 1, maxMouth)..secretMax = maxSecretMouth;
-        leftEye = new SpriteLayer("LeftEye","$folder/LeftEye/", 1, maxEye)..primaryPartner = false;
-        rightEye = new SpriteLayer("RightEye","$folder/RightEye/", 1, maxEye)..partners.add(leftEye);
-        glasses = new SpriteLayer("Glasses","$folder/Glasses/", 1, maxGlass);
-        glasses2 = new SpriteLayer("Glasses2","$folder/Glasses2/", 0, maxGlass2)..secretMax = maxSecretGlass2;
-         */
         double scale = 0.6;
         int smolWidth = (width * scale).round();
         int smolHeight = (height * scale).round();
@@ -86,6 +63,19 @@ class SmolKidDoll extends HomestuckDoll {
 
         leftEye = new PositionedLayerPlusUltra(smolWidth, smolHeight, x,y,"LeftEye","$folder/LeftEye/", 1, maxEye)..primaryPartner = false;
         rightEye = new PositionedLayerPlusUltra(smolWidth, smolHeight,x,y,"RightEye","$folder/RightEye/", 1, maxEye)..partners.add(leftEye);
+
+        extendedHairTop = new PositionedLayerPlusUltra(smolWidth, smolHeight, x,y,"HairFront","$folder/HairTop/", 1, maxHair)..secretMax = maxSecretHair;
+        extendedHairBack = new PositionedLayerPlusUltra(smolWidth, smolHeight, x,y,"HairBack","$folder/HairBack/", 1, maxHair)..secretMax = maxSecretHair;
+        extendedHairBack.syncedWith.add(extendedHairTop);
+        extendedHairTop.syncedWith.add(extendedHairBack);
+        extendedHairBack.slave = true;
+
+        glasses = new PositionedLayerPlusUltra(smolWidth, smolHeight,x,y,"Glasses","$folder/Glasses/", 1, maxGlass);
+        glasses2 = new PositionedLayerPlusUltra(smolWidth, smolHeight,x,y,"Glasses2","$folder/Glasses2/", 0, maxGlass2)..secretMax = maxSecretGlass2;
+
+        mouth = new PositionedLayerPlusUltra(smolWidth, smolHeight,x,y,"Mouth","$folder/Mouth/", 1, maxMouth)..secretMax = maxSecretMouth;
+        symbol = new PositionedLayerPlusUltra(smolWidth, smolHeight,x,y,"Symbol","$folder/Symbol/", 1, maxSymbol)..secretMax = maxSecretSymbol;
+        facePaint = new PositionedLayerPlusUltra(smolWidth, smolHeight,x,y,"FacePaint","$folder/FacePaint/", 0, maxFacePaint);
 
 
     }
