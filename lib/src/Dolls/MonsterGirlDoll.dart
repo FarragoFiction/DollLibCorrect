c638ca1fe3173773ed9128aa05a94feaebe731dd
jadedResearcher <gtg287y@gmail.com>
2019-04-28 09:35:29 -0400

    no numbers in names

--- lib/src/Dolls/MonsterGirlDoll.dart
+++ lib/src/Dolls/MonsterGirlDoll.dart
@@ -46,18 +46,18 @@ class MonsterGirlDoll extends MagicalDoll {
     void initLayers() {
 
         {
-            hairBack = layer("$name.1HeadDecorations", "1HeadDecorations/", 1);
-            bowBack = layer("$name.2Whatever", "2Whatever/", 1);
-            body = layer("$name.3Head", "3Head/", 1);
-            socks = layer("$name.4Arms", "4Arms/", 1);
-            shoes = layer("$name.5Skirts", "5Skirts/", 1);
-            skirt = layer("$name.6Clothing", "6Clothing/", 1);
-            frontBow = layer("$name.7Legs", "7Legs/", 1);
-            eyes = layer("$name.8Torso", "8Torso/", 1);
-            eyebrows = layer("$name.9Capes", "9Capes/", 1);;
-            mouth = layer("$name.10Wings", "10Wings/", 1);
-            hairFront = layer("$name.11Tail", "11Tail/", 1);
-            glasses = layer("$name.12FX", "12FX/", 1);
+            hairBack = layer("$name.HeadDecorations", "1HeadDecorations/", 1);
+            bowBack = layer("$name.Whatever", "2Whatever/", 1);
+            body = layer("$name.Head", "3Head/", 1);
+            socks = layer("$name.Arms", "4Arms/", 1);
+            shoes = layer("$name.Skirts", "5Skirts/", 1);
+            skirt = layer("$name.Clothing", "6Clothing/", 1);
+            frontBow = layer("$name.Legs", "7Legs/", 1);
+            eyes = layer("$name.Torso", "8Torso/", 1);
+            eyebrows = layer("$name.Capes", "9Capes/", 1);;
+            mouth = layer("$name.Wings", "10Wings/", 1);
+            hairFront = layer("$name.Tail", "11Tail/", 1);
+            glasses = layer("$name.FX", "12FX/", 1);
 
 
         }
