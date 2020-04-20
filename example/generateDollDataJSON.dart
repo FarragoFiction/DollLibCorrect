16a46c78e60805ae590fb93927107ff8e4a5be29
jadedResearcher <gtg287y@gmail.com>
2020-04-19 22:50:22 -0400

    non standard json inserted

--- example/generateDollDataJSON.dart
+++ example/generateDollDataJSON.dart
@@ -22,8 +22,68 @@ abstract class DollDataGenerator {
                 ret[doll.name] = new Map<String,dynamic>();
                 ret[doll.name] ["layers"] = doll.toDollDataLayers();
                 ret[doll.name] ["urls"] = doll.toDollDataLayersURLs();
+                if(doll.name == "Troll") {
+                    ret[doll.name]["defaultBody"]= 48;
+                    ret[doll.name]["mutantEyes"]= [2,11,31,44,46,47,85];
+                    ret[doll.name]["bannedBodies"]= [238,252,256,259,235,226,227,230,96,219,221,223,5,11,14,43,50,59,65,66,67,70,72,75,74,98,100,101,102,106,107,109,63,17];
+                }else if(doll.name == "Lamia") {
+                    ret[doll.name]["seaDwellerBodies"]= [7,8,9,12,13,27,28,29,34,35,39,40,46,50,51,52,60,61,78];
+                }else if(doll.name == "Grub") {
+                    ret[doll.name]["landDwellerBodies"]= [0,1,2,3,4,5,6,7,8];
+                    ret[doll.name]["seaDwellerBodies1"]= [9,10,11,12,13,14,15,16,17];
+                    ret[doll.name]["seaDwellerBodies2"]= [18,19,20,21,22,23,24,26,26];
+                    ret[doll.name]["upsideDownBodies"]= [7,8,26,25,16,17];
+                }
+            }else {
+                if(doll.name == "Queen") {
+                    ret[doll.name] = new Map<String,dynamic>();
+                    ret[doll.name]["parts"]= [
+                        "Bird",
+                        "Bug",
+                        "Buggy_As_Fuck_Retro_Game",
+                        "Butler",
+                        "Cat",
+                        "Chihuahua",
+                        "Chinchilla",
+                        "Clippy",
+                        "Cow",
+                        "Cowboy",
+                        "Doctor",
+                        "Dutton",
+                        "Fly",
+                        "Game_Bro",
+                        "Game_Grl",
+                        "Gerbil",
+                        "Github",
+                        "Golfer",
+                        "Google",
+                        "Horse",
+                        "Husky",
+                        "Internet_Troll",
+                        "Kid_Rock",
+                        "Librarian",
+                        "Llama",
+                        "Mosquito",
+                        "Nic_Cage",
+                        "Penguin",
+                        "Pitbull",
+                        "Pomeranian",
+                        "Pony",
+                        "Praying_Mantis",
+                        "Rabbit",
+                        "Robot",
+                        "Sleuth",
+                        "Sloth",
+                        "Tissue",
+                        "Web_Comic_Creator",
+                        "Pigeon",
+                        "Octopus",
+                        "Worm",
+                        "Kitten",
+                        "Fish"
+                        ];
+                }
             }
-
         }
         for(String key in ret.keys) {
             print("key is $key");
